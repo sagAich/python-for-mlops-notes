4d38a9ce2fd52e992146086d4e77142027ac64fd
Sagar Aich <sagaraich01@gmail.com>
2026-06-10 12:23:58 +0530

    done some modifications on args.py.

--- notes-on-args-and-kwargs/args.py
+++ notes-on-args-and-kwargs/args.py
@@ -1,7 +1,8 @@
 #variable length arguments
 # *args:
 # 1. when you do not know the exact number of arguments that will be passed to a function.
-# 2. when you want to pass a list or a tuple as an argument to a function.
+# 2. when you want to pass a list or a tuple as an argument to a function. Inside the function, args will still be a tuple,
+#  regardless of whether you passed a list or a tuple.
 def employee_details(*employee):
     print(f"type inside function: {type(employee)}")
     print(f"Content: {employee}")
@@ -10,3 +11,19 @@ employee_details_list = ["Sagar", "Aich", "Software Engineer"]
 employee_details_tuple = ("Sagar", "Aich", "Software Engineer")
 employee_details(*employee_details_list)
 employee_details(*employee_details_tuple)
+# for employee_details(*employee_details_list), the * operator unpacks the list into individual arguments: Sagar, Aich, Software Engineer
+# Here, *employee collects all positional arguments into a tuple.
+# So internally it becomes:
+# employee = ("Sagar", "Aich", "Software Engineer")
+# for employee_details(*employee_details_tuple), the * operator unpacks the tuple into individual arguments: Sagar, Aich, Software Engineer
+# Here, *employee collects all positional arguments into a tuple.
+# So internally it becomes:
+# employee = ("Sagar", "Aich", "Software Engineer")
+# If you want the function to receive the list itself
+
+# Don't unpack it.
+# def employee_details(employee):
+#     print(type(employee))
+#     print(employee)
+#
+# employee_details(employee_details_list)
